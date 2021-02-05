b186c77c33d1880db4531bbdf49c9f80a3d6ffa9
Noah Gibbs <the.codefolio.guy@gmail.com>
2021-02-05 05:05:10 +0000

    Fix typo in NGinX host header changes

--- dev_scripts/stackscript/linode_stackscript.sh
+++ dev_scripts/stackscript/linode_stackscript.sh
@@ -349,7 +349,7 @@ server {
       proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
       proxy_set_header X-Real-IP \$remote_addr;
       proxy_set_header X-Forwarded-Proto \$scheme;
-      proxy_set_header Host $host;
+      proxy_set_header Host \$host;
       proxy_http_version 1.1;
       proxy_set_header Upgrade \$http_upgrade;
       proxy_set_header Connection \$connection_upgrade;
@@ -657,7 +657,7 @@ server {
       proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
       proxy_set_header X-Real-IP \$remote_addr;
       proxy_set_header X-Forwarded-Proto \$scheme;
-      proxy_set_header Host $host;
+      proxy_set_header Host \$host;
     }
 
     ssl_certificate /etc/letsencrypt/live/$FQDN_CLIENT/fullchain.pem; # managed by Certbot
