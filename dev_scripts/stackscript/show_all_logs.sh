108a8862bb3129b86e59fe88e51a9e8437bc99e6
Noah Gibbs <the.codefolio.guy@gmail.com>
2021-02-05 01:56:08 +0000

    Remove obsolete logfile name from show_all_logs.sh

--- dev_scripts/stackscript/show_all_logs.sh
+++ dev_scripts/stackscript/show_all_logs.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 cd /var/log
-tail -f apache2/client-error.log dgd/* userdb/* dgd_server.out tunnel/gables*.log userdb*.*
+tail -f apache2/client-error.log dgd/* userdb/* tunnel/gables*.log userdb*.*
